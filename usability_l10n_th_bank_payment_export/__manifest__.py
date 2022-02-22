0492f06e14c28d0853f968447ec7add31ed16d8b
Saran440 <saranl@ecosoft.co.th>
2022-02-22 10:23:27 +0700

    [ADD] usability_l10n_th_bank_payment_export

---
+++ usability_l10n_th_bank_payment_export/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Usability Thai Localization - Bank Payment Export",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
+    "depends": ["l10n_th_bank_payment_export"],
+    "data": ["views/bank_payment_config_view.xml"],
+    "installable": True,
+    "maintainers": ["Saran440"],
+    "development_status": "Alpha",
+}
