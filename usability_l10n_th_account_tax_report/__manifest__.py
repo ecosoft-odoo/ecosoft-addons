3b7b2d69978d77146e0bf4db92c9dd3cc5d5602f
ps-tubtim <pimolnats@ecosoft.co.th>
2022-02-22 09:24:42 +0700

    move WHT RD Report to l10n_th

--- usability_l10n_th_account_tax_report/__manifest__.py
+++ usability_l10n_th_account_tax_report/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -9,15 +9,9 @@
     "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
     "depends": [
         "l10n_th_account_tax_report",
-        "l10n_th_fonts",
     ],
     "data": [
-        "security/ir.model.access.csv",
-        "data/paper_format.xml",
-        "data/report_data.xml",
-        "reports/report_rd_withholding_tax_qweb.xml",
         "views/withholding_tax_cert.xml",
-        "wizard/rd_withholding_tax_report_wizard_view.xml",
     ],
     "installable": True,
     "maintainers": ["ps-tubtim"],
