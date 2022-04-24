c6f052567d9d4b98f0390b7ddc3b55b534762670
newtratip <tharathip.chaweewongphan@gmail.com>
2022-04-24 16:14:30 +0700

    [FIX] Add attribute copy=False in name res.partner

---
+++ usability_l10n_th_partner/models/res_partner.py
@@ -0,0 +1,10 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    name = fields.Char(copy=False)
