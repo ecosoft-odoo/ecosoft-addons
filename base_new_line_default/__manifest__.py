842310c4e890bb6e5b3ca32c322c03a5e5487ed3
ps-tubtim <pimolnats@ecosoft.co.th>
2023-01-17 17:16:55 +0700

    Initialize 15.0 branch

---
+++ base_new_line_default/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "New one2many line defaults from previous line",
+    "version": "15.0.1.0.0",
+    "category": "Usability",
+    "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["base"],
+    "demo": ["demo/res_groups.xml"],
+    "maintainers": ["kittiu"],
+}
