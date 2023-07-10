21d998004439d6c709805b1406d07eba08ca1872
Saran440 <saranl@ecosoft.co.th>
2023-07-10 15:23:25 +0700

    [15.0][ADD] stock_account_visible_valuation

---
+++ stock_account_visible_valuation/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Account - Visible Valuation",
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "depends": ["stock_account"],
+    "data": [
+        "views/stock_valuation_layer_views.xml",
+    ],
+    "installable": True,
+}
