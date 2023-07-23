6cc834bf4ba983ec5fdb41e5243a573f392a3a3b
ps-tubtim <pimolnats@ecosoft.co.th>
2023-07-23 12:18:02 +0700

    [ADD] mrp_stock_analytic

---
+++ mrp_stock_analytic/models/stock_move.py
@@ -0,0 +1,8 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
