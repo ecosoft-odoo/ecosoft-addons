efd1c1af02f77118e4459d5b3b0963ff3b749d0c
Saran440 <saranl@ecosoft.co.th>
2023-07-29 15:13:14 +0700

    [FIX] don't allow reset / reverse in expense if paid

---
+++ account_move_reconcile_no_cancel/wizard/account_move_reversal.py
@@ -0,0 +1,28 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import _, models
+from odoo.exceptions import ValidationError
+
+
+class AccountMoveReversal(models.TransientModel):
+    _inherit = "account.move.reversal"
+
+    def reverse_moves(self):
+        self.ensure_one()
+        moves = self.move_ids
+        for rec in moves:
+            # Expense case journal only (not include payment)
+            sheet = rec.line_ids.mapped("expense_id").mapped("sheet_id")
+            sheet_not_post = sheet.filtered(lambda l: l.state != "post")
+            rec_pay_lines = rec._get_receivable_payable_lines()
+            if (
+                rec.move_type in ["in_invoice", "out_invoice"]
+                and (
+                    rec_pay_lines.matched_debit_ids or rec_pay_lines.matched_credit_ids
+                )
+            ) or (sheet and sheet_not_post and not rec.payment_id):
+                raise ValidationError(
+                    _("You cannot reset to draft / reverse reconciled entries.")
+                )
+        return super().reverse_moves()
