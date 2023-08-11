ce36653c2b6705efc8c8e85523d732a45de8f07c
Saran440 <saranl@ecosoft.co.th>
2023-08-11 16:57:45 +0700

    [FIX] create logs after done

--- usability_webhooks/controllers/main.py
+++ usability_webhooks/controllers/main.py
@@ -14,20 +14,23 @@ class WebhookController(http.Controller):
         head = request.httprequest.headers
         request.session.authenticate(head["db"], head["login"], head["password"])
         # Add logs
-        api_log = request.env["api.log"].create(
-            {"data": json.dumps(vals), "model": model, "route": "/api/create_data"}
-        )
+        data_dict = {
+            "data": json.dumps(vals),
+            "model": model,
+            "route": "/api/create_data",
+        }
         # Create Data & Update logs
         try:
             res = request.env["webhook.utils"].create_data(model, vals)
             state = "done" if res["is_success"] else "failed"
-            api_log.write({"result": res, "state": state})
+            data_dict.update({"result": res, "state": state})
         except Exception as e:
             res = {
                 "is_success": False,
                 "messages": e,
             }
-            api_log.write({"result": res, "state": "failed"})
+            data_dict.update({"result": res, "state": "failed"})
+        request.env["api.log"].create(data_dict)
         return res
 
     @http.route("/api/create_update_data", type="json", auth="none")
@@ -35,19 +38,18 @@ class WebhookController(http.Controller):
         head = request.httprequest.headers
         request.session.authenticate(head["db"], head["login"], head["password"])
         # Add logs
-        api_log = request.env["api.log"].create(
-            {
-                "data": json.dumps(vals),
-                "model": model,
-                "route": "/api/create_update_data",
-            }
-        )
+        data_dict = {
+            "data": json.dumps(vals),
+            "model": model,
+            "route": "/api/create_update_data",
+        }
         # Create/Update Data & Update logs
         try:
             res = request.env["webhook.utils"].create_update_data(model, vals)
             state = "done" if res["is_success"] else "failed"
-            api_log.write({"result": res, "state": state})
+            data_dict.update({"result": res, "state": state})
         except Exception as e:
             res = e
-            api_log.write({"result": e, "state": "failed"})
+            data_dict.update({"result": res, "state": "failed"})
+        request.env["api.log"].create(data_dict)
         return res
