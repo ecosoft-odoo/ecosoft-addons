7f997e54c660f9ec99d88fcd7ac88496179e734f
Saran440 <saranl@ecosoft.co.th>
2023-08-15 14:22:48 +0700

    [FIX] rollback api configuration on system parameter

--- usability_webhooks/controllers/main.py
+++ usability_webhooks/controllers/main.py
@@ -20,16 +20,25 @@ class WebhookController(http.Controller):
             "route": "/api/create_data",
         }
         # Create Data & Update logs
+        ICP = self.env["ir.config_parameter"]
+        rollback_state_failed = ICP.sudo().get_param("webhook.rollback_state_failed")
+        rollback_except = ICP.sudo().get_param("webhook.rollback_except")
         try:
             res = request.env["webhook.utils"].create_data(model, vals)
             state = "done" if res["is_success"] else "failed"
             data_dict.update({"result": res, "state": state})
+            # Not success, rollback all data (if config in system parameter)
+            if not res["is_success"] and rollback_state_failed:
+                request.env.cr.rollback()
         except Exception as e:
             res = {
                 "is_success": False,
                 "messages": e,
             }
             data_dict.update({"result": res, "state": "failed"})
+            # Error from odoo exception, rollback all data (if config in system parameter)
+            if rollback_except:
+                request.env.cr.rollback()
         request.env["api.log"].create(data_dict)
         return res
 
@@ -44,15 +53,24 @@ class WebhookController(http.Controller):
             "route": "/api/create_update_data",
         }
         # Create/Update Data & Update logs
+        ICP = self.env["ir.config_parameter"]
+        rollback_state_failed = ICP.sudo().get_param("webhook.rollback_state_failed")
+        rollback_except = ICP.sudo().get_param("webhook.rollback_except")
         try:
             res = request.env["webhook.utils"].create_update_data(model, vals)
             state = "done" if res["is_success"] else "failed"
             data_dict.update({"result": res, "state": state})
+            # Not success, rollback all data (if config in system parameter)
+            if not res["is_success"] and rollback_state_failed:
+                request.env.cr.rollback()
         except Exception as e:
             res = {
                 "is_success": False,
                 "messages": e,
             }
             data_dict.update({"result": res, "state": "failed"})
+            # Error from odoo exception, rollback all data (if config in system parameter)
+            if rollback_except:
+                request.env.cr.rollback()
         request.env["api.log"].create(data_dict)
         return res
