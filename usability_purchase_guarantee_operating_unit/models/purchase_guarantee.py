50bb97f76e5ffe437a7603c6cd8e13c9a31e5aab
ps-tubtim <pimolnat.s@outlook.com>
2023-09-28 11:52:40 +0700

    [FIX] usability_purchase_guarantee_operating_unit

--- usability_purchase_guarantee_operating_unit/models/purchase_guarantee.py
+++ usability_purchase_guarantee_operating_unit/models/purchase_guarantee.py
@@ -11,6 +11,7 @@ class PurchaseGuarantee(models.Model):
     operating_unit_id = fields.Many2one(
         comodel_name="operating.unit",
         compute="_compute_operating_unit",
+        store=True,
         index=True,
     )
 
