78541d62ebf1fdec924dfeb2ae8295e9ebb251ed
TheerayutEncoder <theerayutattajak@gmail.com>
2023-10-08 14:29:30 +0700

    [15.0][UPD] update purpose in data template

--- odoo_etax_inet/models/account_move.py
+++ odoo_etax_inet/models/account_move.py
@@ -7,11 +7,6 @@ class AccountMove(models.Model):
     _name = "account.move"
     _inherit = ["account.move", "etax.th"]
 
-    def _update_created_purpose(self, doc):
-        # if it's credit note
-        # if it's debit note
-        pass
-
     def _get_branch_id(self):
         """
         By default, core odoo do not provide branch_id field in account.move and account.payment.
@@ -21,8 +16,15 @@ class AccountMove(models.Model):
             return self.branch_id
 
     def _prepare_inet_data(self, form_type="", form_name=""):
-        res = super()._prepare_inet_date()
+        res = super()._prepare_inet_data()
         branch_id = self._get_branch_id()
         if branch_id:
-            res.update({"c02_seller_branch_id": branch_id})
+            res.update({"c02_seller_branch_id": branch_id.name})
+        if self.create_purpose_code:
+            res.update(
+                {
+                    "h05_create_purpose_code": self.create_purpose_code,
+                    "h06_create_purpose": self.create_purpose,
+                }
+            )
         return res
