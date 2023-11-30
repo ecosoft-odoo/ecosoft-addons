4b04c2035b3aa21d14e19e61595454d0c08ea198
Kitti U <kittiu@gmail.com>
2023-11-30 22:06:38 +0700

    Add replacement lock date

--- frappe_etax_service/wizards/wizard_select_replacement_purpose.py
+++ frappe_etax_service/wizards/wizard_select_replacement_purpose.py
@@ -1,7 +1,9 @@
 # Copyright 2023 Ecosoft., co.th
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+import datetime
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class WizardSelectReplacementPurpose(models.TransientModel):
@@ -25,6 +27,7 @@ class WizardSelectReplacementPurpose(models.TransientModel):
         active_ids = self.env.context.get("active_ids", [])
         move = self.env["account.move"].browse(active_ids)
         move.ensure_one()
+        self._check_replacement_lock_date(move.invoice_date)
         replacement = move.create_replacement_etax()
         replacement.create_purpose_code = self.purpose_code_id.code
         replacement.create_purpose = self.reason
@@ -37,3 +40,16 @@ class WizardSelectReplacementPurpose(models.TransientModel):
             "res_id": replacement.id,
             "context": self.env.context,
         }
+
+    def _check_replacement_lock_date(self, invoice_date):
+        # Test lock date
+        lock_date = int(
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("frappe_etax_service.replacement_lock_date", 1)
+        )
+        dt = (
+            invoice_date.replace(day=1) + datetime.timedelta(days=32)
+        ).replace(day=lock_date)
+        if datetime.date.today() > dt:
+            raise ValidationError(_("Create Replace e-Tax not allowed after %s") % dt.strftime("%d/%m/%Y"))
