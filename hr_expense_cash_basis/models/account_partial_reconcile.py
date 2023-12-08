20417b1fe568da0321d949e2fa58b6c22395dd08
Saran440 <saranl@ecosoft.co.th>
2023-12-08 17:17:38 +0700

    [FIX] update pre-commit

--- hr_expense_cash_basis/models/account_partial_reconcile.py
+++ hr_expense_cash_basis/models/account_partial_reconcile.py
@@ -1,6 +1,8 @@
 # Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from datetime import date
+
 from odoo import fields, models
 
 
