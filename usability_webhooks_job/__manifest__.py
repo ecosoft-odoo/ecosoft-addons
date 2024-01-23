0c987b61f901617c5ae03fb16c29024d732f3a4a
Saran440 <saranl@ecosoft.co.th>
2024-01-23 14:46:22 +0700

    [16.0][ADD] usability_webhooks_job

---
+++ usability_webhooks_job/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "REST API - Job Queue",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
+    "depends": ["usability_webhooks", "queue_job"],
+}
---
+++ usability_webhooks_job/controllers/utils.py
@@ -0,0 +1,32 @@
+# Copyright 2023 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import _, api, models
+
+
+class WebhookUtils(models.AbstractModel):
+    _inherit = "webhook.utils"
+
+    @api.model
+    def create_data(self, model, vals):
+        if vals.get("run_job_queue"):
+            job = self.with_delay().create_data(model, vals)
+            return {
+                "is_success": True,
+                "job_uuid": job.uuid,
+                "result": {},
+                "messages": _("Record updated successfully"),
+            }
+        return super().create_data(model, vals)
+
+    @api.model
+    def create_update_data(self, model, vals):
+        if vals.get("run_job_queue"):
+            job = self.with_delay().create_update_data(model, vals)
+            return {
+                "is_success": True,
+                "job_uuid": job.uuid,
+                "result": {},
+                "messages": _("Record updated successfully"),
+            }
+        return super().create_update_data(model, vals)
