c3419cf63f4f903c5555c4fa229e277d5178e3ba
theerayut <theerayutattajak@gmail.com>
2025-01-15 13:57:11 +0700

    [ADD] Create replacement etax receipt

--- frappe_etax_service/wizards/wizard_select_replacement_purpose.py
+++ frappe_etax_service/wizards/wizard_select_replacement_purpose.py
@@ -19,6 +19,18 @@ class WizardSelectReplacementPurpose(models.TransientModel):
     reason = fields.Char(
         required=True,
     )
+    res_model = fields.Char(
+        string='Resource Model',
+        default=lambda self: self.env.context.get('default_res_model', '')
+    )
+
+    @api.model
+    def default_get(self, fields):
+        res = super(WizardSelectReplacementPurpose, self).default_get(fields)
+        context = self.env.context
+        if context.get('default_res_model'):
+            res['res_model'] = context.get('default_res_model')
+        return res
 
     @api.onchange("purpose_code_id")
     def _onchange_purpose_code_id(self):
@@ -26,13 +38,35 @@ class WizardSelectReplacementPurpose(models.TransientModel):
 
     def create_replacement(self):
         active_ids = self.env.context.get("active_ids", [])
-        move = self.env["account.move"].browse(active_ids)
+        if self.res_model == 'account.move':
+            move = self.env["account.move"].browse(active_ids)
+            invoice_date = move.invoice_date
+        elif self.res_model == 'account.payment':
+            move = self.env["account.payment"].browse(active_ids)
+            invoice_date = move.date
+
         move.ensure_one()
-        self._check_replacement_lock_date(move.invoice_date)
+
+        self._check_replacement_lock_date(invoice_date)
+
+        # Create replacement name
+        old_number = move.name
+        suffix = "-R"
+        if suffix in old_number:
+            [number, rev] = old_number.split(suffix)
+            replace_name = f"{number}{suffix}{int(rev) + 1}"
+        else:
+            replace_name = f"{old_number}{suffix}{1}"
         replacement = move.create_replacement_etax()
+        replacement.name = replace_name
         replacement.create_purpose_code = self.purpose_code_id.code
         replacement.create_purpose = self.reason
-        replacement.replaced_entry_id = move
+
+        if self.res_model == 'account.move':
+            replacement.replaced_entry_id = move
+        elif self.res_model == 'account.payment':
+            replacement.replaced_receipt_id = move
+
         return {
             "type": "ir.actions.act_window",
             "views": [(False, "form")],
