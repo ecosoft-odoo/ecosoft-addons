31e4c3611dab48f72c3bb0cc9a2cff2e78db09d4
Saran440 <saranl@ecosoft.co.th>
2025-02-27 14:52:24 +0700

    [IMP] account_financial_report_extension: change position

--- account_financial_report_extension/wizard/abstract_wizard.py
+++ account_financial_report_extension/wizard/abstract_wizard.py
@@ -1,19 +1,18 @@
-# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# Copyright 2025 Ecosoft Co., Ltd. (http://ecosoft.co.th)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
 
 
-class GeneralLedgerReportWizard(models.TransientModel):
-    _inherit = "general.ledger.report.wizard"
+class AbstractWizard(models.AbstractModel):
+    _inherit = "account_financial_report_abstract_wizard"
 
     map_type_id = fields.Many2one(
         comodel_name="data.map.type",
-        string="Map Type",
         help="Used to map data for report printing",
     )
 
-    def _prepare_report_general_ledger(self):
-        res = super()._prepare_report_general_ledger()
+    def _prepare_report_data(self):
+        res = super()._prepare_report_aged_partner_balance()
         res.update({"map_type_id": self.map_type_id.id or False})
         return res
