f87444ea44af02d7beff797ead0d02ea9fa01b94
Saran440 <saranl@ecosoft.co.th>
2025-04-11 14:29:40 +0700

    [NOMERGE] reverse code financial report

--- account_financial_report_extension/wizard/__init__.py
+++ account_financial_report_extension/wizard/__init__.py
@@ -1,3 +1,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import abstract_wizard
+from . import aged_partner_balance_wizard
+from . import general_ledger_wizard
+from . import journal_ledger_wizard
+from . import open_items_wizard
+from . import trial_balance_wizard
