77179fb750333f403fedf1eee289e14a5c78b9b6
Saran440 <saranl@ecosoft.co.th>
2025-05-02 16:39:10 +0700

    [IMP] request_document_*: create original document to draft when create request document

--- request_document_expense/models/request_order.py
+++ request_document_expense/models/request_order.py
@@ -4,8 +4,8 @@
 from odoo import api, fields, models
 
 
-class RequestRequest(models.Model):
-    _inherit = "request.request"
+class RequestOrder(models.Model):
+    _inherit = "request.order"
 
     expense_sheet_counts = fields.Integer(
         string="Expense Sheet Count",
