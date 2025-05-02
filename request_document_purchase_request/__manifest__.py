1b2df36c11f1b9e758a18a62a0b7a261adf697ed
Saran440 <saranl@ecosoft.co.th>
2025-05-02 16:39:10 +0700

    [IMP] request_document_*: create original document to draft when create request document

--- request_document_purchase_request/__manifest__.py
+++ request_document_purchase_request/__manifest__.py
@@ -10,11 +10,9 @@
     "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
     "depends": ["request_document", "purchase_request"],
     "data": [
-        "security/ir.model.access.csv",
         "views/res_config_settings_views.xml",
         "views/purchase_request_view.xml",
-        "views/request_document_view.xml",
-        "views/request_request_view.xml",
+        "views/request_order_view.xml",
     ],
     "installable": True,
 }
--- request_document_purchase_request/models/request_document.py
+++ request_document_purchase_request/models/request_document.py
@@ -11,85 +11,10 @@ class RequestDocument(models.Model):
         selection_add=[("purchase_request", "Purchase Request")],
         ondelete={"purchase_request": "cascade"},
     )
-
-    pr_requested_by = fields.Many2one(
-        comodel_name="res.users",
-        string="Requested By",
-        check_company=True,
-        default=lambda self: self.env.user,
-    )
-    pr_description = fields.Text(string="Description")
-    pr_origin = fields.Char(string="Source Document")
-    pr_date_start = fields.Date(
-        string="Creation date",
-        default=fields.Date.context_today,
-    )
-    pr_picking_type_id = fields.Many2one(
-        comodel_name="stock.picking.type",
-        string="Picking Type",
-        required=True,
-        default=lambda self: self._default_picking_type(),
-    )
-    pr_estimated_cost = fields.Monetary(
-        compute="_compute_estimated_cost",
-        string="Total Estimated Cost",
-        store=True,
-    )
-    pr_line_ids = fields.One2many(
-        comodel_name="request.document.purchase.request.line",
-        inverse_name="document_id",
-    )
     purchase_request_ids = fields.One2many(
         comodel_name="purchase.request",
         inverse_name="request_document_id",
     )
-
-    @api.depends("pr_line_ids", "pr_line_ids.estimated_cost")
-    def _compute_estimated_cost(self):
-        for rec in self:
-            rec.pr_estimated_cost = sum(rec.pr_line_ids.mapped("estimated_cost"))
-
-    @api.model
-    def _default_picking_type(self):
-        type_obj = self.env["stock.picking.type"]
-        company_id = self.env.context.get("company_id") or self.env.company.id
-        types = type_obj.search(
-            [("code", "=", "incoming"), ("warehouse_id.company_id", "=", company_id)]
-        )
-        if not types:
-            types = type_obj.search(
-                [("code", "=", "incoming"), ("warehouse_id", "=", False)]
-            )
-        return types[:1]
-
-    def _get_pr_line_values(self):
-        self.ensure_one()
-        pr_line_list = [
-            {
-                "name": line.name,
-                "product_id": line.product_id.id,
-                "product_qty": line.product_qty,
-                "date_required": line.date_required,
-                "estimated_cost": line.estimated_cost,
-            }
-            for line in self.pr_line_ids
-        ]
-        return pr_line_list
-
-    def _get_pr_values(self, pr_line_list):
-        self.ensure_one()
-        return {
-            "name": "New",
-            "requested_by": self.pr_requested_by.id,
-            "origin": self.pr_origin,
-            "description": self.pr_description,
-            "date_start": self.pr_date_start,
-            "picking_type_id": self.pr_picking_type_id.id,
-            "company_id": self.company_id.id,
-            "currency_id": self.currency_id.id,
-            "request_document_id": self.id,
-            "line_ids": [(0, 0, line) for line in pr_line_list],
-        }
 
     def _update_state_purchase_request(self, purchase_request):
         self.ensure_one()
@@ -105,27 +30,39 @@ class RequestDocument(models.Model):
 
     def _create_purchase_request(self):
         self.ensure_one()
-        # Create Purchase Request
-        pr_line_list = self._get_pr_line_values()
-        sheet_dict = self._get_pr_values(pr_line_list)
-        purchase_request = self.env["purchase.request"].create(sheet_dict)
-        self._update_state_purchase_request(purchase_request)
-        return purchase_request
+        return self._update_state_purchase_request(self.purchase_request_ids)
 
+    def open_request_document(self):
+        res = super().open_request_document()
+        if self.request_type == "purchase_request":
+            ctx = self.env.context.copy()
+            ctx.update(
+                {
+                    "default_request_document_id": self.id,
+                    "invisible_header": 1,
+                    "create": 0,  # Not allow create
+                }
+            )
+            if self.state == "draft":
+                ctx["allow_edit"] = 1
 
-class RequestDocumentPurchaseRequestLine(models.Model):
-    _name = "request.document.purchase.request.line"
-    _inherit = "purchase.request.line"
-    _description = "Purchase Request Line"
+            return {
+                "type": "ir.actions.act_window",
+                "views": [(False, "form")],
+                "view_mode": "form",
+                "res_model": "purchase.request",
+                "res_id": self.purchase_request_ids.id,  # should be 1 only
+                "context": ctx,
+                "target": "new",
+            }
+        return res
 
-    document_id = fields.Many2one(
-        comodel_name="request.document",
-        string="Document",
-        ondelete="cascade",
-    )
-    purchase_lines = fields.Many2many(
-        comodel_name="purchase.order.line",
-        relation="document_pr_po_line_rel",
-        column1="document_id",
-        column2="purchase_order_line_id",
-    )
+    @api.depends("purchase_request_ids")
+    def _compute_document(self):
+        res = super()._compute_document()
+        for rec in self:
+            pr_id = rec.purchase_request_ids
+            if pr_id:
+                rec.name_document = pr_id.name
+                rec.total_amount_document = pr_id.estimated_cost
+        return res
