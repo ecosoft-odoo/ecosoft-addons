716d6aca1375b39d3964438857841b10685e381d
Pani-k-folk <Panithank@ecosoft.co.th>
2025-05-19 13:20:25 +0700

    [MIG] request_document_exception: Migration to 18.0

--- request_document_exception/__manifest__.py
+++ request_document_exception/__manifest__.py
@@ -3,13 +3,14 @@
 
 {
     "name": "Request Document - Exception",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
     "depends": ["request_document", "base_exception"],
     "data": [
+        "data/request_exception_data.xml",
         "security/ir.model.access.csv",
         "views/request_order_view.xml",
         "wizard/request_exception_confirm_view.xml",
