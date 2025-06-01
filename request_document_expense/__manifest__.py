e5d4196ce1e9b9933a1da72edf725cf60cc5e4b8
Saran440 <saranl@ecosoft.co.th>
2025-06-01 07:44:54 +0000

    [BOT] post-merge updates

--- request_document_expense/__manifest__.py
+++ request_document_expense/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Request Document - Expense",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "category": "Accounting & Finance",
     "author": "Ecosoft, Odoo Community Association (OCA)",
