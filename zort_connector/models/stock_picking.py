034037f1b7e59e4fc477fa7eb960a0ec249cd7f7
theerayutEncoder <theerayutattajak@gmail.com>
2025-11-10 16:59:20 +0700

    [FIX] zort_connector: change process to update qty to zort

--- zort_connector/models/stock_picking.py
+++ zort_connector/models/stock_picking.py
@@ -25,10 +25,8 @@ class StockPicking(models.Model):
     def button_validate(self):
         res = super().button_validate()
         # Only update qty to zort if the picking is not related to a Zort order
-        if not self.sale_id or not getattr(self.sale_id, "is_zort_order", False):
-            # TODO: Handle API errors gracefully.
-            #       Since button_validate is called after the stock move is done,
-            #       it is safe to sync qty to Zort here.
+        # if not internal transfer update qty to zort
+        if self.picking_type_code not in ("internal", "mrp_operation"):
             self._sync_qty_to_zort()
         return res
 
@@ -38,65 +36,49 @@ class StockPicking(models.Model):
 
     def _sync_qty_to_zort(self):
         """
-        Syncs the quantity of products in Zort based on the stock picking type.
-        Uses picking_type_code to decide whether to increase or decrease stock.
+        Sync the quantity of products in Zort, always update available qty to Zort
+        when click button_validate.
         """
         for picking in self:
-            data = {"stocks": []}
-            # Use move_ids_without_package for outgoing, move_ids for incoming
-            moves = (
-                picking.move_ids
-                if picking.picking_type_code == "incoming"
-                else picking.move_ids_without_package
-            )
-            for move in moves:
-                if move.product_id.is_created_on_zort:
-                    data["stocks"].append(
-                        {
-                            "sku": move.product_id.default_code,
-                            "stock": move.quantity,
-                        }
-                    )
+            try:
+                data = {"stocks": []}
+                # Use move_ids_without_package for outgoing, move_ids for incoming
+                moves = (
+                    picking.move_ids
+                    if picking.picking_type_code == "incoming"
+                    else picking.move_ids_without_package
+                )
+                for move in moves:
+                    if move.product_id.is_created_on_zort:
+                        data["stocks"].append(
+                            {
+                                "sku": move.product_id.default_code,
+                                "stock": move.quantity,
+                            }
+                        )
 
-            if not data.get("stocks"):
-                continue
-            if picking.picking_type_code == "incoming":
+                if not data.get("stocks"):
+                    continue
                 wh_code = (
                     self.env["ir.config_parameter"]
                     .sudo()
                     .get_param("zort_connector.warehouse_code", default="W0001")
                 )
-                response = self._increase_product_stock_list(
-                    warehousecode=wh_code, data=data
+                response = self._update_product_available_stock_list(
+                    warehousecode=wh_code,
+                    data=data,
                 )
-                action = _("increased")
-            elif picking.picking_type_code == "outgoing":
-                response = self._decrease_product_stock_list(
-                    warehousecode=wh_code, data=data
-                )
-                action = _("decreased")
-            else:
-                continue  # Do nothing for other picking types
-
-            if response.get("error"):
-                _logger.error(
-                    "Error {} product stock in Zort: {}".format(
-                        action, response.get("error")
+                if response.get("error"):
+                    _logger.error(
+                        "Error syncing stock to Zort: %s", response.get("error")
                     )
-                )
-                picking.message_post(
-                    body=_(
-                        "Failed to {action} product stock on Zort: {error}".format(
-                            action=action, error=response.get("error")
-                        )
-                    ),
-                    subtype_xmlid="mail.mt_note",
-                )
-            else:
-                self.updated_qty_to_zort = True
-                picking.message_post(
-                    body=_(f"Product stock {action} successfully on Zort."),
-                    subtype_xmlid="mail.mt_note",
+                else:
+                    _logger.info(
+                        "Successfully synced stock to Zort for picking %s", picking.name
+                    )
+            except Exception as e:
+                _logger.error(
+                    "Exception occurred while syncing product stock to Zort: %s", str(e)
                 )
 
     @api.model
