7a62a167c733b831cda5816e15d333bb9a62f8b2
Saran440 <saranl@ecosoft.co.th>
2026-04-29 18:00:17 +0700

    [18.0][ADD] base_hide_action_delete

---
+++ base_hide_action_delete/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2026 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Base Hide Action Delete",
+    "summary": "Hide Delete from the Action menu, configurable per security group",
+    "version": "18.0.1.0.0",
+    "category": "Technical",
+    "author": "Ecosoft",
+    "license": "AGPL-3",
+    "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
+    "depends": ["web"],
+    "data": [
+        "security/base_hide_action_delete_groups.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "base_hide_action_delete/static/src/js/hide_delete_action.esm.js",
+        ],
+    },
+    "installable": True,
+    "maintainers": ["Saran440"],
+}
