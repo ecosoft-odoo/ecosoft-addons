b622207ea79029b4f4cf25108d0e4a13168e7b4e
Saran440 <saranl@ecosoft.co.th>
2026-05-28 19:43:35 +0700

    [18.0][ADD] base_print_wizard

---
+++ base_print_wizard/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base Print Wizard",
+    "version": "18.0.1.0.0",
+    "summary": "Add wizard when use print action",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/ecosoft-odoo/ecosoft-addons",
+    "depends": ["base"],
+    "data": [
+        "security/ir.model.access.csv",
+        "models/ir_actions_report_view.xml",
+        "wizard/base_print_wizard_view.xml",
+    ],
+    "installable": True,
+    "maintainers": ["Saran440"],
+    "development_status": "Alpha",
+}
