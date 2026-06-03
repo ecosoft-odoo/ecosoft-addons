OCA/account-analytic

[FIX] analytic_tag_dimension: Handle dimension removal and module uninstallation
If not, there is remaining garbage in ir.model.fields and SQL columns in tables, avoiding
a second install of the module or to reuse the same dimension code.

analytic_tag_dimension/hooks.py
@@ -1,0 +1,9 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, SUPERUSER_ID
+
+
+def uninstall_hook(cr, registry):
+    """Cleanup all dimensions before uninstalling."""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["account.analytic.dimension"].search([]).unlink()

ecosoft-odoo/ecosoft-addons

analytic_tag_dimension/hooks.py
@@ -1,0 +1,9 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import SUPERUSER_ID, api
+
+
+def uninstall_hook(cr, registry):
+    """Cleanup all dimensions before uninstalling."""
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["account.analytic.dimension"].search([]).unlink()