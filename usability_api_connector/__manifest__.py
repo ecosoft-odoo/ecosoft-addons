9046585ec5819ae4514f0cbc88dd522248f2a0a3
Saran440 <saranl@ecosoft.co.th>
2026-07-26 07:57:57 +0000

    [BOT] post-merge updates

--- usability_api_connector/__manifest__.py
+++ usability_api_connector/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Usability - API Connector",
-    "version": "18.0.1.1.1",
+    "version": "18.0.1.1.2",
     "license": "AGPL-3",
     "category": "Tools",
     "author": "Ecosoft, Odoo Community Association (OCA)",
